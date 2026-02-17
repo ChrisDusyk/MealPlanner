6fe73d034af914b7df8f8abcd1397b16c268368d
Chris Dusyk <chris.dusyk@gmail.com>
2026-02-17 13:56:05 -0600

    feat(apphost): add Keycloak integration with realm import

--- MealPlanner.AppHost/AppHost.cs
+++ MealPlanner.AppHost/AppHost.cs
@@ -4,11 +4,17 @@ var mongoDb = builder.AddMongoDB("mongodb")
 	.WithLifetime(ContainerLifetime.Persistent);
 var mealPlannerDb = mongoDb.AddDatabase("mealplannerDb");
 
+var keycloak = builder.AddKeycloak("keycloak", 8080)
+	.WithRealmImport("./Realms")
+	.WithLifetime(ContainerLifetime.Persistent);
+
 var api = builder.AddProject<Projects.MealPlanner_Api>("api")
-	.WithReference(mealPlannerDb).WaitFor(mealPlannerDb);
+	.WithReference(mealPlannerDb).WaitFor(mealPlannerDb)
+	.WithReference(keycloak).WaitFor(keycloak);
 
 builder.AddViteApp("frontend", "..\\frontend")
 	.WithReference(api).WaitFor(api)
+	.WithReference(keycloak).WaitFor(keycloak)
 	.WithPnpm()
 	.WithEndpoint("http", cfg =>
 	{
