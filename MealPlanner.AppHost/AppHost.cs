9e12c1311fca0ccdf7b520e1451ed068f4abbc11
Chris Dusyk <chris.dusyk@gmail.com>
2026-02-18 17:05:31 -0600

    Add docker compose support

--- MealPlanner.AppHost/AppHost.cs
+++ MealPlanner.AppHost/AppHost.cs
@@ -1,16 +1,21 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
+builder.AddDockerComposeEnvironment("compose");
+
 var mongoDb = builder.AddMongoDB("mongodb")
-	.WithLifetime(ContainerLifetime.Persistent);
+	.WithLifetime(ContainerLifetime.Persistent)
+	.PublishAsDockerComposeService((resource, service) => { service.Name = "mealplannerMongoDb"; });
 var mealPlannerDb = mongoDb.AddDatabase("mealplannerDb");
 
 var keycloak = builder.AddKeycloak("keycloak", 8080)
 	.WithRealmImport("./Realms")
-	.WithLifetime(ContainerLifetime.Persistent);
+	.WithLifetime(ContainerLifetime.Persistent)
+	.PublishAsDockerComposeService((resource, service) => { service.Name = "mealplannerKeycloak"; });
 
 var api = builder.AddProject<Projects.MealPlanner_Api>("api")
 	.WithReference(mealPlannerDb).WaitFor(mealPlannerDb)
-	.WithReference(keycloak).WaitFor(keycloak);
+	.WithReference(keycloak).WaitFor(keycloak)
+	.PublishAsDockerComposeService((resource, service) => { service.Name = "mealplannerApi"; });
 
 builder.AddViteApp("frontend", "..\\frontend")
 	.WithReference(api).WaitFor(api)
@@ -21,6 +26,7 @@ builder.AddViteApp("frontend", "..\\frontend")
 		cfg.Port = 3000;
 		cfg.IsProxied = false;
 		cfg.IsExternal = true;
-	});
+	})
+	.PublishAsDockerComposeService((resource, service) => { service.Name = "mealplannerFrontend"; });
 
 builder.Build().Run();
