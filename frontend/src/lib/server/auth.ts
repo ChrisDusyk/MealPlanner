29c082d712513c5e8359408002d2b8e391d35aaa
Chris Dusyk <chris.dusyk@gmail.com>
2026-04-27 16:22:20 -0600

    Add better auth infra for connection to cloud config

--- frontend/src/lib/server/auth.ts
+++ frontend/src/lib/server/auth.ts
@@ -2,7 +2,8 @@ import { sveltekitCookies } from 'better-auth/svelte-kit';
 import { getRequestEvent } from '$app/server';
 import { building } from '$app/environment';
 import { createAuth } from './auth-options';
+import { dash } from '@better-auth/infra';
 
-export const auth = createAuth([sveltekitCookies(getRequestEvent)], {
-	allowMissingConnectionString: building
+export const auth = createAuth([sveltekitCookies(getRequestEvent), dash()], {
+	allowMissingConnectionString: building,
 });
